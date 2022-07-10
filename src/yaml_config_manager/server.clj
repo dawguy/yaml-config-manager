75a8a9e4eac30aa4c48eaa25a691ebd2cfead023
David Wright <hockeyguy411@gmail.com>
2022-07-10 08:47:45 -0400

    Middleware json wrappers for ring response and request.

--- src/yaml_config_manager/server.clj
+++ src/yaml_config_manager/server.clj
@@ -1,23 +1,32 @@
 (ns yaml-config-manager.server
   (:use [ring.adapter.jetty])
-  (:require [clojure.data.json]))
+  (:require [clojure.data.json]
+            [cheshire.core :as json]
+            [ring.middleware.json :refer [wrap-json-body wrap-json-response]]
+            ))
 
 (use 'yaml-config-manager.server :reload-all)
 
 (def re (atom {}))
+(def re-body (atom {}))
 (def server (atom nil))
-
-
 
 (defn handler [request]
   (do
     (reset! re request)
-    {:status 200
-   :headers {"Content-Type" "application/json"}
-   :body "{'response': 'woot woot'"}))
+    (reset! re-body request)
+    {:status  200
+     :headers {"Content-Type" "application/json"}
+     :body    {:body "woot woot!" :a (:server-name request)}}))
+
+(defn app [request]
+  ((-> handler
+       wrap-json-body
+       wrap-json-response) request)
+  )
 
 (defn h [request]
-  (handler request))
+  (app request))
 
 (defn start []
   (reset! server (run-jetty h {:port 3000 :join? false})))
